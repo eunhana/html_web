c2f7a4cfc0bc68954887cc937ac15faf91f36836
eunhana <133560072+eunhana@users.noreply.github.com>
2023-06-07 02:54:57 +0900

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,54 @@
+var Links = {
+  setColor:function(color){
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i < alist.length){
+      alist[i].style.color = color;
+      i = i + 1;
+    }
+  }
+}
+var Body = {
+  setColor:function (color){
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor:function (color){
+    document.querySelector('body').style.backgroundColor = color;
+  }
+}
+function nightDayHandler(self){
+  var target = document.querySelector('body');
+  if(self.value === 'night'){
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+
+    Links.setColor('white');
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+
+    Links.setColor('blue');
+  }
+}
+
+var scrollToTopBtn = document.getElementById('scroll-to-top-btn');
+/*var scrollToBottomBtn = document.getElementById('scroll-to-bottom-btn');*/
+
+var scrollToTopBtn = document.documentElement
+function scrollToTop() {
+  scrollToTopBtn.scrollTo({
+    top: 0,
+    behavior: "smooth"
+})}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+/*var scrollToBottomBtn = document.documentElement
+function scrollToBot() {
+  scrollToTopBtn.scrollTo({
+    top: document.documentElement.scrollHeight,
+    behavior: "smooth"
+})}
+scrollToBottomBtn.addEventListener("click", scrollToBot);*/
+  
